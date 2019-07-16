28811a52bac15417bbe8de2dc66c73b78153ff88
Yingxuan Guo <yingxuan.guo9771@gmail.com>
2019-07-16 16:20:27 +0800

    added processor for stanfordnlp pipeline

--- stanfordnlp_demo.py
+++ stanfordnlp_demo.py
@@ -1,8 +1,10 @@
 import stanfordnlp
 
 # Simple demo of StanfordNLP Python package
-nlp = stanfordnlp.Pipeline()
+nlp = stanfordnlp.Pipeline(processors='tokenize,pos,depparse',
+                           use_gpu=True)
 
 doc = nlp("Barack Obama was born in Hawaii.  He was elected president in 2008.")
 
-doc.sentences[0].print_dependencies()
+for sentence in doc.sentences:
+    sentence.print_dependencies()
